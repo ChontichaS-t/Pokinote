fbc4772395e1b85422140bcf6e2e5e5556aebe8a
ChontichaS-t <chontichasukchalee@gmail.com>
2026-07-06 21:57:30 +0700

    Delete

--- src/app/todolist/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function TodoListPage() {
-  return (
-    <main className="flex flex-col min-h-screen items-center justify-center bg-white gap-30">
-      
-      <h1>หน้า Todo List เปล่า</h1>
-
-    </main>
-  );
-}
