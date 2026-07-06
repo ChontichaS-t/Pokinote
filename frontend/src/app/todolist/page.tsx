b381e65a32859e9071b2c5a60c038cf5937819e0
ChontichaS-t <chontichasukchalee@gmail.com>
2026-07-06 21:48:39 +0700

    feat: restructure project with frontend and backend

---
+++ frontend/src/app/todolist/page.tsx
@@ -0,0 +1,9 @@
+export default function TodoListPage() {
+  return (
+    <main className="flex flex-col min-h-screen items-center justify-center bg-white gap-30">
+      
+      <h1>หน้า Todo List เปล่า</h1>
+
+    </main>
+  );
+}
